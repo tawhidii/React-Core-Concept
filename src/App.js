83588e745753bd7025ca0a846b2286016c7336cd
barii <khademul.py@gmail.com>
2021-07-19 13:47:24 +0600

    :triangular_flag_on_post: Understanding Component >> 1

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let superHeroOne = {
+    name:'SuperMan',
+    comics: 'DC',
+    producer : 'Jack Snyder'
+  }
+
+  let style = {
+    color : 'red',
+    backgroundColor:'white'
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <br></br>This is edited..
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={style}> Movie Name : {superHeroOne.name}</h1>
+      <h2 style={{color:"cyan",backgroundColor:"yellow"}}>Producer Name: {superHeroOne.producer}</h2>
+      <ActionMovie></ActionMovie>
+      </header> 
     </div>
   );
 }
 
+function ActionMovie(){
+  const movieStyle = {
+    border: '2px solid red',
+    margin: '10px',
+    backgroundColor:'green'
+  }
+  return(
+    <div style={movieStyle}>
+       <h1> Movie Name : Fast and Furious </h1>
+       <h3> Producer Name : Jack Frost </h3>
+    </div>
+  ) 
+}
 export default App;
